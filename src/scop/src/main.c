440a0fbf2141b36b28e291053227a7edacebabc2
Guillaume Bersac <bersac_1@hotmail.fr>
2015-07-16 12:45:50 +0200

    background is not glitching anymore

--- src/scop/src/main.c
+++ src/scop/src/main.c
@@ -50,7 +50,6 @@ int main()
 	// Create and compile our GLSL program from the shaders
 	t_shader *shader1 = load_shader("src/shaders/couleur2d.vert",
 			"src/shaders/couleur2d.frag");
-	// GLuint programID = shader1->program_id;
 	if (shader1 == NULL)
 	{
 		//TODO free stuff
@@ -67,58 +66,35 @@ int main()
 	glGenBuffers(1, &vertexbuffer);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-
 	do{
 		// Clear the screen
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT);
 
 		// Use our shader
-		// glUseProgram(progID);
 		glUseProgram(shader1->program_id);
 
 		// 1rst attribute buffer : vertices
 		glEnableVertexAttribArray(0);
 		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
 		glVertexAttribPointer(
-		        0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-		        3,                  // size
-		        GL_FLOAT,           // type
-		        GL_FALSE,           // normalized?
-		        0,                  // stride
-		        (void*)0            // array buffer offset
+			0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+			3,                  // size
+			GL_FLOAT,           // type
+			GL_FALSE,           // normalized?
+			0,                  // stride
+			(void*)0            // array buffer offset
 		);
 
 		// Draw the triangle !
 		glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
-
 		glDisableVertexAttribArray(0);
 
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-
-		// 1rst attribute buffer : vertices
-		glEnableVertexAttribArray(0);
-		glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-		glVertexAttribPointer(
-				0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
-				3,                  // size
-				GL_FLOAT,           // type
-				GL_FALSE,           // normalized?
-				0,                  // stride
-				(void*)0);          // array buffer offset
-
-		// Draw the triangle !
-		glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
-
-		glDisableVertexAttribArray(0);
-
-		// Swap buffers
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	} // Check if the ESC key was pressed or the window was closed
-	 while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
-			glfwWindowShouldClose(window) == 0 );
+	}
+	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
+		   glfwWindowShouldClose(window) == 0 );
 
 	// Cleanup VBO
 	glDeleteBuffers(1, &vertexbuffer);
